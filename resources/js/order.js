54232e8b8a820356eabb777fcb8578ef9b3d380a
Nando <ernandofebrian6@gmail.com>
2025-10-08 11:42:50 +0700

    Add Orders page views, update product/header views, and making if statement in header partials

--- resources/js/order.js
+++ resources/js/order.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeBtn = document.querySelector('.close-button');
 
     closeBtn.addEventListener('click', () => {
-        window.location.href = './index.html';
+        window.location.href = '/';
     });
 
     if (placeOrderBtn && modal) {
